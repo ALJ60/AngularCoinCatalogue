14042a70e94b3865a0f96a1c7de52cfa0019606c
Andrew Jones <andrew@alj.me.uk>
2019-05-05 14:55:58 +0100

    Sort order added to collection table

--- database/procedures/collection/Collection_SelectAll.sql
+++ database/procedures/collection/Collection_SelectAll.sql
@@ -1,8 +1,8 @@
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Collection_SelectAll`()
-  READS SQL DATA
-  COMMENT 'Returns all collections'
+    READS SQL DATA
+    COMMENT 'Returns all collections'
 SELECT *
 	FROM collection
-  ORDER BY Collection$$
+    ORDER BY SortOrder$$
 DELIMITER ;
--- database/tables/collection.sql
+++ database/tables/collection.sql
@@ -1,6 +1,13 @@
 CREATE TABLE `collection` (
-  `CollectionId` INT NOT NULL AUTO_INCREMENT,
-  `Collection` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`CollectionId`),
-  UNIQUE (`Collection`))
-  ENGINE = InnoDB;
+  `CollectionId` int(11) NOT NULL,
+  `Collection` varchar(50) NOT NULL,
+  `SortOrder` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `collection`
+  ADD PRIMARY KEY (`CollectionId`),
+  ADD UNIQUE KEY `Collection` (`Collection`);
+
+ALTER TABLE `collection`
+  MODIFY `CollectionId` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;

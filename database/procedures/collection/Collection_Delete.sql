da973c96d88bdb18c0f7a42ccb7b4dab8c8f05e6
Andrew Jones <andrew@alj.me.uk>
2019-09-29 15:55:20 +0100

    Error message added when trying to delete collection used in an album.

--- database/procedures/collection/Collection_Delete.sql
+++ database/procedures/collection/Collection_Delete.sql
@@ -1,6 +1,18 @@
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Collection_Delete`(IN `p_CollectionId` INT)
     MODIFIES SQL DATA
-DELETE FROM collection
-	WHERE CollectionId = p_CollectionId$$
+BEGIN
+
+  IF EXISTS(SELECT *
+              FROM album
+              WHERE CollectionId = p_CollectionId
+            ) THEN
+    SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Cannot delete - used for albums';
+  END IF;
+
+  DELETE FROM collection
+    WHERE CollectionId = p_CollectionId;
+    
+END$$
 DELIMITER ;
